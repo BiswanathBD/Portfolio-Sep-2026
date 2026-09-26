d6e9b153b457e0684ec0e4408d0c4a5343a932da
BiswanathBD <biswanath.sarker.bd@gmail.com>
2026-09-26 22:30:33 +0600

    fixed touch issue

--- src/utils/SmoothScroll.tsx
+++ src/utils/SmoothScroll.tsx
@@ -10,11 +10,12 @@ gsap.registerPlugin(ScrollTrigger);
 export default function SmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.5,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 2,
       smoothWheel: true,
+      syncTouch: true,
+      touchInertiaExponent: 1,
       wheelMultiplier: 1,
-      touchMultiplier: 1.5,
+      touchMultiplier: 1,
     });
 
     lenis.on("scroll", ScrollTrigger.update);
